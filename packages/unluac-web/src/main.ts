df44880ec786f4277ee406415a18f0689b6f7cd7
x3zvawq <x3zvawq@gmail.com>
2026-04-17 22:06:16 +0800

    chore(web): 移除废弃代码

--- packages/unluac-web/src/main.ts
+++ packages/unluac-web/src/main.ts
@@ -1,7 +1,6 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import { restoreFromUrl } from '@/composables/useShareUrl'
-import { injectClarity } from '@/utils/clarity'
 import App from './App.vue'
 import { i18n } from './i18n'
 import '@/styles/main.css'
@@ -15,6 +14,3 @@ app.use(i18n)
 restoreFromUrl()
 
 app.mount('#app')
-
-// 非开发环境自动注入 Clarity（injectClarity 内部已做 DEV 检测）
-injectClarity()
